344b27981e9d521b98ff7ce82994e23f2fbe58c1
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-10-09 06:32:12 +0200

    nuova query per ottenere la corrispondenza scuola->zona

--- school_subscriptions.py
+++ school_subscriptions.py
@@ -3,46 +3,68 @@ import os
 from api import Api
 from mycsv import csv_header, csv_row
 
-query = """query SubscriptionsTable($editionId: String!, $after: String, $filter: SchoolSubscriptionFilter, $order: SchoolSubscriptionOrder) {
+query = """query SubscriptionsWithZones($editionId: String!, $after: String, $filter: SchoolSubscriptionFilter, $order: SchoolSubscriptionOrder) {
   olympiads {
     edition(id: $editionId) {
-      subscriptions(after: $after, filters: $filter, order: $order) {
-        ...SubscriptionTableFragment
-        edges {
-          node {
-            status
-            donation
-            __typename
-          }
-          __typename
-        }
+      olympiad {
+        id
+        name
         __typename
       }
-      __typename
-    }
-    __typename
-  }
-}
-
-fragment SubscriptionTableFragment on SchoolSubscriptionTypeConnection {
-  pageInfo {
-    hasNextPage
-    endCursor
-    __typename
-  }
-  totalCount
-  edges {
-    node {
-      isValid
-      school {
-        ...SchoolFragment
-        location {
-          city {
-            province {
-              region {
+      subscriptions(after: $after, filters: $filter, order: $order) {
+        pageInfo {
+          hasNextPage
+          endCursor
+          __typename
+        }
+        totalCount
+        edges {
+          node {
+            isValid
+            status
+            donation
+            school {
+              id
+              externalId
+              name
+              type {
                 name
                 __typename
               }
+              email
+              isActive
+              gameOnly
+              location {
+                address
+                postalCode
+                city {
+                  id
+                  name
+                  province {
+                    id
+                    name
+                    region {
+                      name
+                      __typename
+                    }
+                    __typename
+                  }
+                  __typename
+                }
+                __typename
+              }
+              __typename
+            }
+            contact {
+              user {
+                name
+                surname
+                email
+                phoneNumber
+                __typename
+              }
+              name
+              surname
               __typename
             }
             __typename
@@ -51,59 +73,10 @@ fragment SubscriptionTableFragment on SchoolSubscriptionTypeConnection {
         }
         __typename
       }
-      contact {
-        user {
-          name
-          surname
-          email
-          phoneNumber
-          __typename
-        }
-        name
-        surname
-        __typename
-      }
       __typename
     }
     __typename
   }
-  __typename
-}
-
-fragment SchoolFragment on SchoolType {
-  id
-  externalId
-  name
-  type {
-    name
-    isLower
-    isMiddle
-    isHigher
-    __typename
-  }
-  email
-  isActive
-  gameOnly
-  location {
-    address
-    postalCode
-    city {
-      id
-      name
-      province {
-        id
-        name
-        region {
-          name
-          __typename
-        }
-        __typename
-      }
-      __typename
-    }
-    __typename
-  }
-  __typename
 }"""
 
 if __name__ == "__main__":
@@ -115,10 +88,98 @@ if __name__ == "__main__":
     api = Api()
     api.login()
     
-    filter = None  # Filter criteria, if needed
-    order = None  # Order criteria, if needed
+    # First, get the olympiad ID from the edition
+    edition_query = """query GetEditionOlympiad($editionId: String!) {
+      olympiads {
+        edition(id: $editionId) {
+          id
+          olympiad {
+            id
+            name
+          }
+        }
+      }
+    }"""
+    
+    edition_response = api.query(edition_query, {"editionId": edition_id})
+    
+    errors = edition_response.get("errors")
+    if errors:
+        print(f"Error getting edition info: {errors}", file=sys.stderr)
+        exit(1)
+    
+    edition_data = edition_response['data']['olympiads']['edition']
+    if not edition_data:
+        print(f"Edition {edition_id} not found", file=sys.stderr)
+        exit(1)
+    
+    olympiad_id = edition_data['olympiad']['id']
+    olympiad_name = edition_data['olympiad']['name']
+    
+    # Get all zones for this olympiad to create a mapping
+    zones_query = """query GetZones($olympiadId: String!) {
+      zones {
+        zones(filters: { olympiad: { id: { exact: $olympiadId } } }) {
+          id
+          name
+          provinces {
+            id
+          }
+          admins {
+            isPrimary
+            user {
+              name
+              surname
+              email
+              phoneNumber
+            }
+          }
+        }
+      }
+    }"""
+    
+    zones_response = api.query(zones_query, {"olympiadId": olympiad_id})
+    
+    errors = zones_response.get("errors")
+    if errors:
+        print(f"Error getting zones info: {errors}", file=sys.stderr)
+        exit(1)
+    
+    # Create a mapping from province_id to zone info
+    province_to_zone = {}
+    zones_data = zones_response['data']['zones']['zones']
+    
+    for zone in zones_data:
+        zone_id = zone['id']
+        zone_name = zone['name']
+        zone_admins = zone['admins']
+        
+        # Find primary admin
+        primary_admin = None
+        for admin in zone_admins:
+            if admin['isPrimary']:
+                primary_admin = admin
+                break
+        
+        # If no primary admin, take the first one
+        if not primary_admin and zone_admins:
+            primary_admin = zone_admins[0]
+        
+        zone_info = {
+            'id': zone_id,
+            'name': zone_name,
+            'admin_name': primary_admin['user']['name'] + ' ' + primary_admin['user']['surname'] if primary_admin else None,
+            'admin_email': primary_admin['user']['email'] if primary_admin else None,
+            'admin_phone': primary_admin['user']['phoneNumber'] if primary_admin else None
+        }
+        
+        # Map each province to this zone
+        for province in zone['provinces']:
+            province_to_zone[province['id']] = zone_info
 
     fields = [
+        "zone_id", "zone_name", 
+        "zone_admin_primary_name", "zone_admin_primary_email", "zone_admin_primary_phone",
         "school_id", "school_external_id", "school_name", "school_type", "school_email", 
         "school_is_active", "school_game_only", "address", "postal_code", 
         "city_id", "city_name", "province_id", "province_name", "region_name",
@@ -126,17 +187,19 @@ if __name__ == "__main__":
         "is_valid", "status", "donation"
     ]
     
+    print(f"Using editionId={edition_id}, olympiadId={olympiad_id} ({olympiad_name})", file=sys.stderr)
+    print(csv_header(fields))
+    
     cursor = None
     hasNextPage = True
-    print(f"Using editionId={edition_id}", file=sys.stderr)
-    print(csv_header(fields))
+    total_schools = 0
     
     while hasNextPage:
         response = api.query(query, {
             "editionId": edition_id,
             "after": cursor,
-            "filter": filter,
-            "order": order
+            "filter": None,
+            "order": None
         })
         
         errors = response.get("errors")
@@ -162,30 +225,46 @@ if __name__ == "__main__":
             province = city['province']
             region = province['region']
             school_type = school['type']
-            user = contact['user']
+            user = contact['user'] if contact else None
+            
+            # Get zone info from the mapping
+            zone_info = province_to_zone.get(province['id'])
+            zone_id = zone_info['id'] if zone_info else None
+            zone_name = zone_info['name'] if zone_info else None
+            zone_admin_name = zone_info['admin_name'] if zone_info else None
+            zone_admin_email = zone_info['admin_email'] if zone_info else None
+            zone_admin_phone = zone_info['admin_phone'] if zone_info else None
             
             # Create a flat row object for csv_row function with all available data
             row_data = {
+                'zone_id': zone_id,
+                'zone_name': zone_name,
+                'zone_admin_primary_name': zone_admin_name,
+                'zone_admin_primary_email': zone_admin_email,
+                'zone_admin_primary_phone': zone_admin_phone,
                 'school_id': school['id'],
                 'school_external_id': school['externalId'],
                 'school_name': school['name'],
-                'school_type': school_type['name'],
+                'school_type': school_type['name'] if school_type else None,
                 'school_email': school['email'],
                 'school_is_active': school['isActive'],
                 'school_game_only': school['gameOnly'],
-                'address': location['address'],
-                'postal_code': location['postalCode'],
-                'city_id': city['id'],
-                'city_name': city['name'],
-                'province_id': province['id'],
-                'province_name': province['name'],
-                'region_name': region['name'],
-                'contact_name': user['name'],
-                'contact_surname': user['surname'],
-                'contact_email': user['email'],
-                'contact_phone': user['phoneNumber'],
+                'address': location['address'] if location else None,
+                'postal_code': location['postalCode'] if location else None,
+                'city_id': city['id'] if city else None,
+                'city_name': city['name'] if city else None,
+                'province_id': province['id'] if province else None,
+                'province_name': province['name'] if province else None,
+                'region_name': region['name'] if region else None,
+                'contact_name': user['name'] if user else None,
+                'contact_surname': user['surname'] if user else None,
+                'contact_email': user['email'] if user else None,
+                'contact_phone': user['phoneNumber'] if user else None,
                 'is_valid': node['isValid'],
                 'status': node['status'],
                 'donation': node['donation']
             }
             print(csv_row(row_data, fields))
+            total_schools += 1
+    
+    print(f"Total schools found: {total_schools}", file=sys.stderr)
