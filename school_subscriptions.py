58b26115bb561f03145fd8b1e0027a95ffa96562
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-10-13 19:13:47 +0200

    aggiunge venue_id all'elenco delle scuole

--- school_subscriptions.py
+++ school_subscriptions.py
@@ -3,7 +3,7 @@ import os
 from api import Api
 from mycsv import csv_header, csv_row
 
-query = """query SubscriptionsWithZones($editionId: String!, $after: String, $filter: SchoolSubscriptionFilter, $order: SchoolSubscriptionOrder) {
+query = """query Subscriptions($editionId: String!, $after: String, $filter: SchoolSubscriptionFilter, $order: SchoolSubscriptionOrder) {
   olympiads {
     edition(id: $editionId) {
       olympiad {
@@ -23,6 +23,10 @@ query = """query SubscriptionsWithZones($editionId: String!, $after: String, $fi
             isValid
             status
             donation
+            schoolVenue {
+              id
+              name
+            }
             school {
               id
               externalId
@@ -88,98 +92,8 @@ if __name__ == "__main__":
     api = Api()
     api.login()
     
-    # First, get the olympiad ID from the edition
-    edition_query = """query GetEditionOlympiad($editionId: String!) {
-      olympiads {
-        edition(id: $editionId) {
-          id
-          olympiad {
-            id
-            name
-          }
-        }
-      }
-    }"""
-    
-    edition_response = api.query(edition_query, {"editionId": edition_id})
-    
-    errors = edition_response.get("errors")
-    if errors:
-        print(f"Error getting edition info: {errors}", file=sys.stderr)
-        exit(1)
-    
-    edition_data = edition_response['data']['olympiads']['edition']
-    if not edition_data:
-        print(f"Edition {edition_id} not found", file=sys.stderr)
-        exit(1)
-    
-    olympiad_id = edition_data['olympiad']['id']
-    olympiad_name = edition_data['olympiad']['name']
-    
-    # Get all zones for this olympiad to create a mapping
-    zones_query = """query GetZones($olympiadId: String!) {
-      zones {
-        zones(filters: { olympiad: { id: { exact: $olympiadId } } }) {
-          id
-          name
-          provinces {
-            id
-          }
-          admins {
-            isPrimary
-            user {
-              name
-              surname
-              email
-              phoneNumber
-            }
-          }
-        }
-      }
-    }"""
-    
-    zones_response = api.query(zones_query, {"olympiadId": olympiad_id})
-    
-    errors = zones_response.get("errors")
-    if errors:
-        print(f"Error getting zones info: {errors}", file=sys.stderr)
-        exit(1)
-    
-    # Create a mapping from province_id to zone info
-    province_to_zone = {}
-    zones_data = zones_response['data']['zones']['zones']
-    
-    for zone in zones_data:
-        zone_id = zone['id']
-        zone_name = zone['name']
-        zone_admins = zone['admins']
-        
-        # Find primary admin
-        primary_admin = None
-        for admin in zone_admins:
-            if admin['isPrimary']:
-                primary_admin = admin
-                break
-        
-        # If no primary admin, take the first one
-        if not primary_admin and zone_admins:
-            primary_admin = zone_admins[0]
-        
-        zone_info = {
-            'id': zone_id,
-            'name': zone_name,
-            'admin_name': primary_admin['user']['name'] + ' ' + primary_admin['user']['surname'] if primary_admin else None,
-            'admin_email': primary_admin['user']['email'] if primary_admin else None,
-            'admin_phone': primary_admin['user']['phoneNumber'] if primary_admin else None
-        }
-        
-        # Map each province to this zone
-        for province in zone['provinces']:
-            province_to_zone[province['id']] = zone_info
-
     fields = [
-        "zone_id", "zone_name", 
-        "zone_admin_primary_name", "zone_admin_primary_email", "zone_admin_primary_phone",
+        "venue_id", "venue_name",
         "school_id", "school_external_id", "school_name", "school_type", "school_email", 
         "school_is_active", "school_game_only", "address", "postal_code", 
         "city_id", "city_name", "province_id", "province_name", "region_name",
@@ -187,7 +101,7 @@ if __name__ == "__main__":
         "is_valid", "status", "donation"
     ]
     
-    print(f"Using editionId={edition_id}, olympiadId={olympiad_id} ({olympiad_name})", file=sys.stderr)
+    print(f"Using editionId={edition_id}", file=sys.stderr)
     print(csv_header(fields))
     
     cursor = None
@@ -226,22 +140,18 @@ if __name__ == "__main__":
             region = province['region']
             school_type = school['type']
             user = contact['user'] if contact else None
-            
-            # Get zone info from the mapping
-            zone_info = province_to_zone.get(province['id'])
-            zone_id = zone_info['id'] if zone_info else None
-            zone_name = zone_info['name'] if zone_info else None
-            zone_admin_name = zone_info['admin_name'] if zone_info else None
-            zone_admin_email = zone_info['admin_email'] if zone_info else None
-            zone_admin_phone = zone_info['admin_phone'] if zone_info else None
+
+            # Gestione schoolVenue come lista
+            school_venue = node['schoolVenue']
+            if school_venue:
+                [venue] = school_venue  # solleva ValueError se la lista ha != 1 elemento
+            else:
+                venue = None
             
             # Create a flat row object for csv_row function with all available data
             row_data = {
-                'zone_id': zone_id,
-                'zone_name': zone_name,
-                'zone_admin_primary_name': zone_admin_name,
-                'zone_admin_primary_email': zone_admin_email,
-                'zone_admin_primary_phone': zone_admin_phone,
+                'venue_id': venue['id'] if venue else None,
+                'venue_name': venue['name'] if venue else None,
                 'school_id': school['id'],
                 'school_external_id': school['externalId'],
                 'school_name': school['name'],
