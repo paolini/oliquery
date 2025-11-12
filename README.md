f05a471534ef1f7477f03eaa998734902543a2d2
Emanuele Paolini <emanuele.paolini@gmail.com>
2025-11-12 09:20:04 +0100

    script importazione archimede

--- README.md
+++ README.md
@@ -41,28 +41,7 @@ python school_subscriptions.py olimat25
 
 # importazione dati per gara di Archimede
 
-Estrai l'elenco delle scuole che si sono iscritte all'edizione di quest'anno
-```
-python school_subscriptions.py olimat26 > school_subscriptions_26.csv
-```
-
-Determina il codice della gara di febbraio le cui `venue` corrispondono
-ai distretti:
-```
-python contests.py
-```
-
-Estrai l'elenco dei distretti e delle loro scuole
-```
-python venues.py 27 > venues_27.csv
-```
-
-Unisci le informazioni in un unico file:
-```
-python merge_schools_coordinators.py school_subscriptions_26.csv venues_27.csv > school_subscriptions_26_with_venue.csv
-```
-
-Importa l'ultimo file ottenuto su olifogli.
+vedi il file README_ARCHIMEDE.md
 
 # Importazione partecipanti (match_or_create_participant)
 
---
+++ archimede_26.sh
@@ -0,0 +1,43 @@
+#/bin/bash
+# script per generare il file delle iscrizioni scolastiche per Archimede 26
+
+set -e
+set -u
+
+export OLI_EDITION=olimat26
+CONTEST_ID=27
+
+echo "OLI_EDITION=${OLI_EDITION}"
+echo "CONTEST_ID=${CONTEST_ID}"
+echo "(per elencare i contest esegui: python contests.py)"
+
+TIMESTAMP=$(date +%Y-%m-%d_%H-%M)
+SCUOLE="${TIMESTAMP}_schools_2026.csv"
+SEDI="${TIMESTAMP}_venues_2026.csv"
+OUT="${TIMESTAMP}_olifogli_2026.csv"
+
+# scarica elenco scuole iscritte
+if [ -f "${SCUOLE}" ]; then
+    echo "File ${SCUOLE} già esistente. Rimuovilo se vuoi rigenerarlo."
+    exit 1
+else
+    python school_subscriptions.py olimat26 > "${SCUOLE}".partial
+    mv "${SCUOLE}".partial "${SCUOLE}"
+    echo "File generato: ${SCUOLE}"
+fi
+
+# scarica elenco sedi di gara
+if [ -f "${SEDI}" ]; then
+    echo "File ${SEDI} già esistente. Rimuovilo se vuoi rigenerarlo."
+    exit 1
+else
+    python venues.py 27 > "${SEDI}".partial
+    mv "${SEDI}".partial "${SEDI}"
+    wc --lines "${SEDI}"
+    echo "File generato: ${SEDI}"
+fi
+
+# unisce i due file
+python merge_schools_coordinators.py "${SCUOLE}" "${SEDI}" > "${OUT}".partial
+mv "${OUT}".partial "${OUT}"
+echo "File generato: ${OUT}"
